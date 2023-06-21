a65e4492caaeba7dd94335fa9797f1c388fadadf
Kimita Wanjohi <kimitawanjohi7923@gmail.com>
2023-06-21 09:04:35 +0100

    Add medical histories and medical treatements tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,3 +14,23 @@ CREATE TABLE
     type VARCHAR(120),
     PRIMARY KEY(id)
   );
+
+CREATE TABLE
+  medical_histories(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at date,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(120),
+    PRIMARY KEY(id)
+  );
+
+
+CREATE TABLE
+  medical_treatments(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    medical_history_id INT REFERENCES medical_histories(id),
+    treatment_id INT REFERENCES treatments(id),
+    PRIMARY KEY(id)
+  );
+
+
