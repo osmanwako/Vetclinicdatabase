c767e034ba578d0eb3249807d4804838850aa1f3
Oumaima Nejjari <oumanj1998@gmail.com>
2023-06-21 15:44:05 +0100

    Create indexes for foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -51,3 +51,10 @@ CREATE TABLE
     total_price DECIMAL,
     PRIMARY KEY(id)
   );
+
+CREATE INDEX ON medical_histories (patient_id);
+CREATE INDEX ON invoices (medical_history_id);
+CREATE INDEX ON invoice_items (invoice_id);
+CREATE INDEX ON invoice_items (treatment_id);
+CREATE INDEX ON medical_histories_has_treatments (medical_history_id);
+CREATE INDEX ON medical_histories_has_treatments (treatment_id);
